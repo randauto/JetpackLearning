03f0269c91f646e01e54a76a7f97a9a5c234f842
tuanlq <tuanlqhvu@gmail.com>
2020-11-02 16:31:19 +0700

    add button text

--- app/src/main/java/com/tuan/learn/MainActivity.kt
+++ app/src/main/java/com/tuan/learn/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.tuan.learn
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.Dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            ScrollableColumn(contentPadding = PaddingValues(Dp(8f), Dp(0f), Dp(0f), Dp(8f))) {
+                val context = ContextAmbient.current
+                Divider(color = Color.Black)
+                Button(onClick = {
+                    val intent = Intent(this@MainActivity, MaterialButtonActivity::class.java)
+                    startActivity(intent)
+                }) {
+                    Text(text = "Material Button")
+                }
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/com/tuan/learn/MaterialButtonActivity.kt
@@ -0,0 +1,10 @@
+package com.tuan.learn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MaterialButtonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
